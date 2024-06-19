0ddd0b31dca494fe737a93dde4668ba3a8d050ac
Sebastian Van Rooyen <95648511+SebastianDevelops@users.noreply.github.com>
2024-06-19 21:15:31 +0200

    Added basic telegram bot (#2)
    * Added basic telegram bot
    
    * Added addition app.config to ignore
    
    * remove bot call for now

---
+++ TelegramBot/MrMeeSeeks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using Telegram.Bot;
+
+
+namespace TelegramBot
+{
+    public class MrMeeSeeks
+    {
+        private readonly string? _botToken;
+
+        public MrMeeSeeks()
+        {
+            _botToken = ConfigurationManager.AppSettings["bot_token"];
+        }
+
+        public async Task Speak()
+        {
+            if(_botToken != null)
+            {
+                var botClient = new TelegramBotClient(_botToken);
+
+                var me = await botClient.GetMeAsync();
+                Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+            }
+        }
+    }
+}
